e7a405b9b17e5dcc88b8d46a0c8be7686aebe756
Irina Novik <novikirishka7@gmail.com>
2024-09-18 21:10:51 +0300

    Add LoginPageXpath and HomePageXpath

---
+++ src/main/java/by/onliner/irinanovik/HomePageXpath.java
@@ -0,0 +1,5 @@
+package by.onliner.irinanovik;
+
+public class HomePageXpath {
+    public static final String buttonLoginXpath= "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
+}
--- src/main/java/by/onliner/irinanovik/LoginPageXPath.java
+++ src/main/java/by/onliner/irinanovik/LoginPageXPath.java
@@ -1,6 +1,6 @@
 package by.onliner.irinanovik;
 
-public class LoginPage {
+public class LoginPageXPath {
     public static final String inputEmailXpath = "//*[@id=\"auth-container";
     public static final String inputPasswordXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div > div > input\")";
     public static final String inputEnterXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div.auth-form__control.auth-form__control_condensed-additional > button\")";
