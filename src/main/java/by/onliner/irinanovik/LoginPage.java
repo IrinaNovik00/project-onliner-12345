f273285186073ae3a38a7a45a3c764934a2a0a90
Irina Novik <novikirishka7@gmail.com>
2024-09-18 20:30:14 +0300

    Refactor HomePage

--- src/main/java/by/onliner/irinanovik/LoginPage.java
+++ src/main/java/by/onliner/irinanovik/LoginPage.java
@@ -5,4 +5,5 @@ public class LoginPage {
     public static final String inputPasswordXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div > div > input\")";
     public static final String inputEnterXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div.auth-form__control.auth-form__control_condensed-additional > button\")";
     public static final String inputErrorEmailXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div:nth-child(1) > div > input\")";
-    public static final String inputErrorPasswordXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div:nth-child(1) > div > input\")"
+    public static final String inputErrorPasswordXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div:nth-child(1) > div > input\")";
+}
