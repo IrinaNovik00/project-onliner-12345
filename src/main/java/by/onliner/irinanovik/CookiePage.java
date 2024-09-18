8dde42c43eb28447dc4bee1890bfe54bfcdecf18
Irina Novik <novikirishka7@gmail.com>
2024-09-18 23:06:57 +0300

    Add LoginTest and refactor the others

---
+++ src/main/java/by/onliner/irinanovik/CookiePage.java
@@ -0,0 +1,12 @@
+package by.onliner.irinanovik;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CookiePage {
+    Webdriver driver;
+    public void clickButtonCloseCookie(Webdriver driver) {
+
+        By cli
+    }
+}
--- src/test/java/by/onliner/irinanovik/LoginTest.java
+++ src/test/java/by/onliner/irinanovik/LoginTest.java
@@ -1,4 +1,18 @@
 package by.onliner.irinanovik;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+
+    @Test
+    public void testLoginWithEmptyData() {
+        WebDriver driver = new ChromeDriver(driver);
+        driver.get ("https://www.onliner.by/");
+        HomePage homePage = new HomePage(driver);
+        homePage.clickButtonLogin();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickButtonEnter(driver);
+    }
 }
