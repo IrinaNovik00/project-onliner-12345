74afc5af12734a9f23cb6ca3e440c66fe89d6ef6
Irina Novik <novikirishka7@gmail.com>
2024-09-18 20:24:23 +0300

    Add HomePage

--- src/main/java/by/onliner/irinanovik/LoginPage.java
+++ src/main/java/by/onliner/irinanovik/LoginPage.java
@@ -1,4 +1,8 @@
 package by.onliner.irinanovik;
 
 public class LoginPage {
-}
+    public static final String inputEmailXpath = "//*[@id=\"auth-container";
+    public static final String inputPasswordXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div > div > input\")";
+    public static final String inputEnterXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div.auth-form__control.auth-form__control_condensed-additional > button\")";
+    public static final String inputErrorEmailXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div:nth-child(1) > div > input\")";
+    public static final String inputErrorPasswordXpath = "document.querySelector(\"#auth-container > div > div.auth-form__body > div > form > div:nth-child(2) > div > div > div:nth-child(1) > div > input\")"
